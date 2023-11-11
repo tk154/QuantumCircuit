d8a6ddc305747ac24a30503dff4157f0d8ae49a1
Til Kaiser <til.kaiser@gmx.de>
2023-11-11 20:04:31 +0100

    Add comments to example.py
    Signed-off-by: Til Kaiser <til.kaiser@gmx.de>

--- example.py
+++ example.py
@@ -6,7 +6,11 @@ initial_qubits = (1, 0, 1)
 circuit = QuantumCircuit(initial_qubits, 
     comment=f"Create circuit with {len(initial_qubits)} qubits")
 
+circuit.comment("Start of QFT", multiline=True)
 circuit.qft()
+circuit.comment("End of QFT", multiline=True)
+
+circuit.empty_line()
 
 circuit.comment("Measure the qubits")
 circuit.measure()
